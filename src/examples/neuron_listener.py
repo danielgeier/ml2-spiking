57a805b91d95f94e65e74d4d381a00da847115e6
Daniel Geier <daniel.geier@student.kit.edu>
2016-06-14 21:26:34 +0200

    Tweak driving parameters.

--- src/examples/neuron_listener.py
+++ src/examples/neuron_listener.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # PKG = 'dvs_simulation'
 from __future__ import division
+from __future__ import division
+
+import sys
 from Queue import Queue
 from time import sleep
 
-
-import rospy
-import sys
 import cv2
-from geometry_msgs.msg import Vector3
-from sensor_msgs.msg import Image, CameraInfo
-import numpy as np
-from cv_bridge import CvBridge, CvBridgeError
-from pyNN.nest import Population, SpikeSourcePoisson, SpikeSourceArray, AllToAllConnector, run, setup, IF_curr_alpha, \
-    end, reset
-from pyNN.nest.projections import Projection
-
 import matplotlib.pyplot as plt
+import numpy as np
+import rospy
+from cv_bridge import CvBridge
+from geometry_msgs.msg import Vector3
+from pyNN.nest import Population, AllToAllConnector, run, setup, IF_curr_alpha, \
+    end
+from pyNN.nest.projections import Projection
+from sensor_msgs.msg import Image
 
 
 class NeuronListener:
@@ -31,21 +31,20 @@ class NeuronListener:
         self.sum_spikes_l = 0
         self.sum_spikes_r = 0
 
-
         self.image_sub = rospy.Subscriber("/dvs/events", Image, self.process_image, queue_size=1)
         self.aer_pub = rospy.Publisher("/AADC_AudiTT/carUpdate", Vector3)
         #  expoisson.all_cells_source.
 
         # Orginal: NE = 960 * 1280
-        NE = 30 * 100 / 2.0
+        NE = 20 * 100 / 2.0
 
         setup(timestep=0.1)
 
-        self.pop_in_l = Population((30, 50), IF_curr_alpha, {'i_offset': np.zeros(NE)})
-        self.pop_in_r = Population((30, 50), IF_curr_alpha, {'i_offset': np.zeros(NE)})
+        self.pop_in_l = Population((20, 50), IF_curr_alpha, {'i_offset': np.zeros(NE)})
+        self.pop_in_r = Population((20, 50), IF_curr_alpha, {'i_offset': np.zeros(NE)})
 
-        self.pop_out_l = Population(1, IF_curr_alpha, {'tau_refrac': 1, 'v_thresh' : -50})
-        self.pop_out_r = Population(1, IF_curr_alpha, {'tau_refrac': 1, 'v_thresh' : -50})
+        self.pop_out_l = Population(1, IF_curr_alpha, {'tau_refrac': 0.5, 'v_thresh': 2500})
+        self.pop_out_r = Population(1, IF_curr_alpha, {'tau_refrac': 0.5, 'v_thresh': 2500})
 
         projection_l = Projection(self.pop_in_l, self.pop_out_l, AllToAllConnector())
         projection_r = Projection(self.pop_in_r, self.pop_out_r, AllToAllConnector())
@@ -57,9 +56,8 @@ class NeuronListener:
         self.pop_out_l.record('spikes')
         self.pop_out_r.record('spikes')
 
-
     def process_image(self, image):
-        #print 'Bild ist da!'
+        # print 'Bild ist da!'
         frame = self.bridge.imgmsg_to_cv2(image)
         # Set the image_data to pop
         cv2.imshow('graublau', frame)
@@ -69,11 +67,9 @@ class NeuronListener:
 
         self.frame_queue.put(frame)
 
-
-
     def inject(self, frame):
-        frame_l = frame[20:50, 0:50]
-        frame_r = frame[20:50, 50:100]
+        frame_l = frame[30:50, 0:50]
+        frame_r = frame[30:50, 50:100]
 
         cv2.imshow('lala', frame_l)
         cv2.waitKey(1)
@@ -86,7 +82,7 @@ class NeuronListener:
         tstop = 100.0
         run(tstop)
 
-        #spikes_in = self.pop_in.get_data()
+        # spikes_in = self.pop_in.get_data()
         data_out_l = self.pop_out_l.get_data()
         data_out_r = self.pop_out_r.get_data()
 
@@ -98,11 +94,13 @@ class NeuronListener:
         self.sum_spikes_l += num_spikes_l
         self.sum_spikes_r += num_spikes_r
 
-        num_spikes_diff = float(num_spikes_l) -float(num_spikes_r)
-        print num_spikes_l, num_spikes_r
-        #print num_spikes_diff
+        num_spikes_diff = num_spikes_l - num_spikes_r
+        speed = 0.5 + 1 / (np.abs(num_spikes_diff / 5.) + 1.01)
 
-        return Vector3(0.3, 0.0, num_spikes_diff) # nach rechts lenken
+        print num_spikes_l, num_spikes_r
+        print 'diff=', num_spikes_diff, 'speed=', speed
+
+        return Vector3(speed, 0., num_spikes_diff / 91.)  # nach rechts lenken
 
     def cleanup(self):
         print "Shutting down vision node."
@@ -125,7 +123,6 @@ def plot_signal(signal, index, colour='b'):
     plt.ylabel("%s (%s)" % (signal.name, signal.units._dimensionality.string))
     plt.setp(plt.gca().get_xticklabels(), visible=False)
     plt.legend()
-
 
 
 def main(args):
